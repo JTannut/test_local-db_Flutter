0a8f025a1338af9b6963883050db148f198237e5
JTannut <tannut_tawonsan@hotmail.com>
2021-04-08 13:09:58 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:checkpoint3/providers/transaction_provider.dart';
+import 'package:checkpoint3/screens/form_screen.dart';
+import 'package:checkpoint3/screens/home_screen.dart';
+import 'package:provider/provider.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) {
+          return TransactionProvider();
+        }),
+      ],
+      child: MaterialApp(
+        title: 'localDB_test',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: MyHomePage(title: 'Warinthon'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title,this.lastname}) : super(key: key);
+
+  final String title,lastname;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: TabBarView(
+          children: [HomeScreen(), FormScreen()],
+        ),
+        bottomNavigationBar: TabBar(
+          tabs: [
+            Tab(
+              icon: Icon(Icons.list),
+              text: "List",
+            ),
+            Tab(icon: Icon(Icons.add), text: "Add")
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/models/Transactions.dart
@@ -0,0 +1,20 @@
+
+class Transactions {
+  String title; //ชื่อ
+  String lastname;
+  String address;
+  // String phone;
+  double amount; //จำนวนเงิน
+  String email;
+  String password;
+  DateTime date; //วันที่ เวลา บันทึกรายการ
+
+  Transactions(
+      {this.title,
+        this.lastname,
+        this.address,
+        this.amount,
+        this.email,
+        this.password,
+        this.date});
+}
